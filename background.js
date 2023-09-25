9a53b1252256e4287386d4f58c1ed320baac11af
takigavin <gavinhorner0@gmail.com>
2023-09-24 21:21:07 -0500

    Generic timer which allows for sitting almost working.

--- background.js
+++ background.js
@@ -1,25 +1,46 @@
-chrome.alarms.onAlarm.addListener(async (alarm) => {
-  await updateTimes()
-})
 
-async function updateTimes(){  
-  let { currStandingHours, currStandingMinutes } =
-    await chrome.storage.session.get([
-      "currStandingHours",
-      "currStandingMinutes",
-    ]);
-  if(currStandingMinutes > 0){
-    currStandingMinutes--;
-  } else if(currStandingMinutes === 0 && currStandingHours > 1){
-    currStandingMinutes = 60
-    currStandingHours--;
-  } 
-  else{
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+  await updateTimes(alarm.name);
+});
+
+async function updateTimes(alarmName) {
+  let ttHours, ttMinutes;
+  if (alarmName === "stand-up") {
+    ttHours = "'currStandingHours'";
+    ttMinutes = "'currStandingMinutes'";
+  } else if (alarmName === "sit-down") {
+    ttHours = "'currSittingHours'";
+    ttMinutes = "'currSittingMinutes'";
+  }
+
+  let timeArr = [{ ttHours }, { ttMinutes }];
+  // generic way of getting this data doesn't seem to be working.
+  let { timeHours, timeMinutes } = await chrome.storage.session.get([
+    ttHours,
+    ttMinutes,
+  ]);
+
+  console.log(timeHours);
+  if (timeMinutes > 0) {
+    timeMinutes--;
+  } else if (timeMinutes === 0 && timeHours > 1) {
+    timeMinutes = 60;
+    timeHours--;
+  } else {
+    // Clear standing timer. Swap to sitting timer.
     chrome.alarms.clearAll();
+
+    chrome.action.setBadgeText({ text: "SIT" });
+
+    chrome.alarms.create("sit-down", {
+      periodInMinutes: 0.1,
+    });
+
     // Somehow reset popup.html to the home view.
   }
 
-  await chrome.storage.session.set(
-    { currStandingHours: currStandingHours, currStandingMinutes: currStandingMinutes },
-  )
+  await chrome.storage.session.set({
+    [{ ttHours }]: timeHours,
+    [{ ttMinutes }]: timeMinutes,
+  });
 }
--- popup/popup.js
+++ popup/popup.js
@@ -1,19 +1,4 @@
-
-async function displayTimerNotActive() {
-  const { loadTimer } = await chrome.storage.sync.get(["loadTimer"]);
-
-  if (loadTimer) {
-    await setCurrentTimes();
-    document.getElementById("pre-standing-blob").style.display = "block";
-  } else if (!loadTimer) {
-    document.getElementById("newUser").innerHTML = "hidden";
-  }
-}
-
-function displayTimerActive() {
-  document.getElementById("standing-blob").style.display = "block";
-}
-
+// Determine the initial loaded html based on timer presence.
 chrome.alarms.getAll((alarms) => {
   if (alarms.length === 0) {
     displayTimerNotActive();
@@ -33,18 +18,6 @@ document.querySelector("#go-to-options").addEventListener("click", function () {
 document.querySelector("#stand-up").addEventListener("click", () => {
   startTimer();
 });
-
-function startTimer() {
-  chrome.action.setBadgeText({ text: "ON" });
-
-  chrome.alarms.create("stand-up", {
-    periodInMinutes: 0.1,
-  });
-
-  document.getElementById("pre-standing-blob").style.display = "none";
-  document.getElementById("standing-blob").style.display = "block";
-  //window.close();
-}
 
 document.querySelector("#end-session").addEventListener("click", function () {
   chrome.action.setBadgeText({ text: "" });
@@ -66,29 +39,92 @@ chrome.storage.onChanged.addListener(async () => {
   ).textContent = `${currStandingHours} hours and ${currStandingMinutes} minutes.`;
 });
 
-async function setCurrentTimes() {
-  const { prefStandingMinutes } = await chrome.storage.sync.get([
-    "prefStandingMinutes",
+
+//#region: Functions
+
+// Used to display html related there not being an ongoing timer.
+async function displayTimerNotActive() {
+  const { loadTimer } = await chrome.storage.sync.get(["loadTimer"]);
+
+  if (loadTimer) {
+    // Existing preferences
+    await setTimes();
+    document.getElementById("pre-standing-blob").style.display = "block";
+  } else if (!loadTimer) {
+    // Nonexisting preferences
+    document.getElementById("newUser").innerHTML = "hidden";
+  }
+}
+
+// Used to display html related to there being an ongoing timer.
+function displayTimerActive() {
+  document.getElementById("standing-blob").style.display = "block";
+}
+
+// Used to start the timer and update the display accordingly.
+function startTimer() {
+  chrome.action.setBadgeText({ text: "STND" });
+
+  chrome.alarms.create("stand-up", {
+    periodInMinutes: 0.1,
+  });
+
+  testStuff();
+
+  document.getElementById("pre-standing-blob").style.display = "none";
+  document.getElementById("standing-blob").style.display = "block";
+  //window.close();
+}
+
+// Used to set both initial and current that are displayed.
+async function setTimes() {
+  const { prefStandingMinutes, prefSittingMinutes } = await chrome.storage.sync.get([
+    "prefStandingMinutes", "prefSittingMinutes",
   ]);
 
-  let standingHours, standingMinutes;
-  standingHours = standingMinutes = 0;
+  let standingHours, standingMinutes, sittingHours, sittingMinutes;
+  standingHours = standingMinutes = sittingHours = sittingMinutes = 0;
 
   if (prefStandingMinutes > 60)
     standingHours = Math.floor(prefStandingMinutes / 60);
 
   standingMinutes = prefStandingMinutes % 60;
 
+  if (prefSittingMinutes > 60)
+    sittingHours = Math.floor(prefSittingMinutes / 60);
+
+  sittingMinutes = prefSittingMinutes % 60;
   
   document.getElementById(
     "pref-standing-time"
   ).textContent = `${standingHours} hours and ${standingMinutes} minutes.`;
+
+  document.getElementById(
+    "pref-sitting-time"
+  ).textContent = `${sittingHours} hours and ${sittingMinutes} minutes.`;
+
   document.getElementById(
     "standing-time"
   ).textContent = `${standingHours} hours and ${standingMinutes} minutes.`;
 
+  document.getElementById(
+    "sitting-time"
+  ).textContent = `${sittingHours} hours and ${sittingMinutes} minutes.`;
+
   await chrome.storage.session.set({
     currStandingHours: standingHours,
     currStandingMinutes: standingMinutes,
+    currSittingHours: sittingHours,
+    currSittingMinutes: sittingMinutes
   });
+}
+
+//#endregion
+
+async function testStuff() {
+  let alarm = await chrome.alarms.getAll();
+  console.log(alarm);
+
+  let storage = await chrome.storage.session.get();
+  console.log(storage);
 }
